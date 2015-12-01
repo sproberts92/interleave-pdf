20115684ea5ab52e0c51f43fd85aa9945560d103
Samuel Roberts <sproberts92@outlook.com>
2015-12-02 00:13:53 +0100

    Replace formlayout GUI with tkinter
    Separate buttons for selecting input and output, and for running
    the interleave procedure.

--- interleave-pdf.py
+++ interleave-pdf.py
@@ -1,25 +1,46 @@
 import PyPDF2
-from formlayout import fedit
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+from tkinter.filedialog import asksaveasfilename
 
-def main():
-	paths = [('Input', ''), ('Output', '')]
+class Application(Frame):
+	def __init__(self):
+		self.input_path = None;
+		self.output_path = None;
 
-	pathsRead = fedit(paths, 
-	                  title="Interleave pdf",
-	                  comment="Enter the full path to the source pdf and a path to output the result."
-	                  )
-	# Full path to files should be specified eg C:\Users\Sam\Documents\Input.pdf and C:\Users\Sam\Documents\Input.pdf
+		Frame.__init__(self)
 
-	document = PyPDF2.PdfFileReader(pathsRead[0])
-	writer = PyPDF2.PdfFileWriter()
+		self.master.resizable(False, False)
+		self.master.title('Interleave PDF')
+		self.grid()
 
-	for page in document.pages:
-		writer.addPage(page)
-		writer.addBlankPage()
+		self.button = Button(self, text="Select input", command=self.load_file, width=12)
+		self.button.grid(row=1, column=0, sticky=W)
 
-	outputStream = open(pathsRead[1], 'wb')
-	writer.write(outputStream)
-	outputStream.close()
+		self.button = Button(self, text="Select output", command=self.save_file, width=12)
+		self.button.grid(row=1, column=2, sticky=W)
+
+		self.button = Button(self, text="Interleave", command=self.interleave, width=12)
+		self.button.grid(row=1, column=3, sticky=W)
+
+	def load_file(self):
+		self.input_path = askopenfilename(filetypes=(("Adobe PDF Files", "*.pdf"), ("All files", "*.*")))
+
+	def save_file(self):
+		self.output_path = asksaveasfilename(filetypes=(("Adobe PDF Files", "*.pdf"), ("All files", "*.*")))
+
+	def interleave(self):
+		if self.input_path and self.output_path:
+			document = PyPDF2.PdfFileReader(self.input_path)
+			writer = PyPDF2.PdfFileWriter()
+
+			for page in document.pages:
+				writer.addPage(page)
+				writer.addBlankPage()
+
+			outputStream = open(self.output_path, 'wb')
+			writer.write(outputStream)
+			outputStream.close()		
 
 if __name__ == "__main__":
-	main()
+	Application().mainloop()
