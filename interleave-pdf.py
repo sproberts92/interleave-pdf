423095d8c8a716bab45d1bdf805cb07583ddef21
Samuel Roberts <sproberts92@outlook.com>
2015-12-02 00:35:08 +0100

    Improve namespace scope for imports
    Reduce chance of name clutter by avoiding "from ... import ...".

--- interleave-pdf.py
+++ interleave-pdf.py
@@ -1,33 +1,32 @@
 import PyPDF2
-from tkinter import *
-from tkinter.filedialog import askopenfilename
-from tkinter.filedialog import asksaveasfilename
+import tkinter as tk
+import tkinter.filedialog as fd
 
-class Application(Frame):
-	def __init__(self):
+class Application(tk.Frame):
+	def __init__(self, master=None):
 		self.input_path = None;
 		self.output_path = None;
 
-		Frame.__init__(self)
+		tk.Frame.__init__(self, master)
 
 		self.master.resizable(False, False)
 		self.master.title('Interleave PDF')
 		self.grid()
 
-		self.button = Button(self, text="Select input", command=self.load_file, width=12)
-		self.button.grid(row=1, column=0, sticky=W)
+		self.button = tk.Button(self, text="Select input", command=self.load_file, width=12)
+		self.button.grid(row=1, column=0)
 
-		self.button = Button(self, text="Select output", command=self.save_file, width=12)
-		self.button.grid(row=1, column=2, sticky=W)
+		self.button = tk.Button(self, text="Select output", command=self.save_file, width=12)
+		self.button.grid(row=1, column=2)
 
-		self.button = Button(self, text="Interleave", command=self.interleave, width=12)
-		self.button.grid(row=1, column=3, sticky=W)
+		self.button = tk.Button(self, text="Interleave", command=self.interleave, width=12)
+		self.button.grid(row=1, column=3)
 
 	def load_file(self):
-		self.input_path = askopenfilename(filetypes=(("Adobe PDF Files", "*.pdf"), ("All files", "*.*")))
+		self.input_path = fd.askopenfilename(filetypes=(("Adobe PDF Files", "*.pdf"), ("All files", "*.*")))
 
 	def save_file(self):
-		self.output_path = asksaveasfilename(filetypes=(("Adobe PDF Files", "*.pdf"), ("All files", "*.*")))
+		self.output_path = fd.asksaveasfilename(filetypes=(("Adobe PDF Files", "*.pdf"), ("All files", "*.*")))
 
 	def interleave(self):
 		if self.input_path and self.output_path:
