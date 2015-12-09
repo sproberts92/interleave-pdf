452896299d8c1492b8d205e6dd45b6f1f359d858
Samuel Roberts <sproberts92@outlook.com>
2015-12-09 21:51:18 +0100

    Add entry widgets for paths
    Can either enter into entry widget or browse. After browsing the
    entry widget is updated with the result from the browse window.

--- interleave-pdf.py
+++ interleave-pdf.py
@@ -13,22 +13,43 @@ class Application(tk.Frame):
 		self.master.title('Interleave PDF')
 		self.grid()
 
-		self.button = tk.Button(self, text="Select input", command=self.load_file, width=12)
-		self.button.grid(row=1, column=0)
+		self.label = tk.Label(self, text="Input", width=12)
+		self.label.grid(row=0, column=0)
 
-		self.button = tk.Button(self, text="Select output", command=self.save_file, width=12)
+		self.entry_in = tk.Entry(self, width=50, textvariable=self.input_path)
+		self.entry_in.grid(row=0, column=1)
+		self.entry_in.insert("end","Path...")
+
+		self.button = tk.Button(self, text="Browse", command=self.load_file, width=12)
+		self.button.grid(row=0, column=2)
+
+		self.label = tk.Label(self, text="Output", width=12)
+		self.label.grid(row=1, column=0)
+
+		self.entry_out = tk.Entry(self, width=50, textvariable=self.output_path)
+		self.entry_out.grid(row=1, column=1)
+		self.entry_out.insert("end","Path...")
+		
+		self.button = tk.Button(self, text="Browse", command=self.save_file, width=12)
 		self.button.grid(row=1, column=2)
 
 		self.button = tk.Button(self, text="Interleave", command=self.interleave, width=12)
-		self.button.grid(row=1, column=3)
+		self.button.grid(row=2, column=2)
 
 	def load_file(self):
 		self.input_path = fd.askopenfilename(filetypes=(("Adobe PDF Files", "*.pdf"), ("All files", "*.*")))
-
+		self.entry_in.delete(0, "end")
+		self.entry_in.insert("end",self.input_path)
+		
 	def save_file(self):
 		self.output_path = fd.asksaveasfilename(filetypes=(("Adobe PDF Files", "*.pdf"), ("All files", "*.*")))
+		self.entry_out.delete(0, "end")
+		self.entry_out.insert("end",self.output_path)
 
 	def interleave(self):
+		self.input_path = self.entry_in.get()
+		self.output_path = self.entry_out.get()
+
 		if self.input_path and self.output_path:
 			document = PyPDF2.PdfFileReader(self.input_path)
 			writer = PyPDF2.PdfFileWriter()
