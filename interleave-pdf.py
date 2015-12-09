4cb2b529c7c1c4ce21fa0e25ebe231449ef30276
Samuel Roberts <sproberts92@outlook.com>
2015-12-09 22:57:19 +0100

    Add ttk for themed widgets
    Just using default style for now.

--- interleave-pdf.py
+++ interleave-pdf.py
@@ -1,38 +1,40 @@
 import PyPDF2
 import tkinter as tk
+import tkinter.ttk as ttk
 import tkinter.filedialog as fd
 
 class Application(tk.Frame):
 	def __init__(self, master=None):
 		tk.Tk()
+
 		self.input_path = tk.StringVar();
 		self.output_path = tk.StringVar();
 
-		tk.Frame.__init__(self, master)
+		ttk.Frame.__init__(self, master)
 
 		self.master.resizable(False, False)
 		self.master.title('Interleave PDF')
 		self.grid()
 
-		self.label = tk.Label(self, text="Input", width=12)
+		self.label = ttk.Label(self, text="Input", width=12, anchor=tk.CENTER)
 		self.label.grid(row=0, column=0)
 
-		self.entry_in = tk.Entry(self, width=50, textvariable=self.input_path)
+		self.entry_in = ttk.Entry(self, width=50, textvariable=self.input_path)
 		self.entry_in.grid(row=0, column=1)
 
-		self.button = tk.Button(self, text="Browse", command=self.load_file, width=12)
+		self.button = ttk.Button(self, text="Browse", command=self.load_file, width=12)
 		self.button.grid(row=0, column=2)
 
-		self.label = tk.Label(self, text="Output", width=12)
+		self.label = ttk.Label(self, text="Output", width=12, anchor=tk.CENTER)
 		self.label.grid(row=1, column=0)
 
-		self.entry_out = tk.Entry(self, width=50, textvariable=self.output_path)
+		self.entry_out = ttk.Entry(self, width=50, textvariable=self.output_path)
 		self.entry_out.grid(row=1, column=1)
 
-		self.button = tk.Button(self, text="Browse", command=self.save_file, width=12)
+		self.button = ttk.Button(self, text="Browse", command=self.save_file, width=12)
 		self.button.grid(row=1, column=2)
 
-		self.button = tk.Button(self, text="Interleave", command=self.interleave, width=12)
+		self.button = ttk.Button(self, text="Interleave", command=self.interleave, width=12)
 		self.button.grid(row=2, column=2)
 
 	def load_file(self):
